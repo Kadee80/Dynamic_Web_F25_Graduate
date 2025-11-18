fa8e983f683677618b1409d65ae179dcea16f46a
Kadee80 <kaa328@nyu.edu>
2025-11-18 14:01:42 -0500

    Add missing reducer code.

---
+++ Week11/Use_Reducer/In_Class_Exercise/Use_Reducer_Hook/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import {Link} from 'react-router-dom'
+
+import Panel from './Panel'
+
+const Navbar = () => {
+  // const activeClass = 'text-blue-500 font-bold decoration-solid'
+  // const pendingClass = 'text-red-500'
+
+  return (
+    <Panel className="sticky top-0 overflow-y-scroll flex flex-col item-start">
+      <Link to="/" className="text-blue-500">
+        {' '}
+        Buttons
+      </Link>
+      <Link to="/accordion" className="text-blue-500">
+        Accordion
+      </Link>
+      <Link to="/dropdown" className="text-blue-500">
+        Dropdown
+      </Link>
+      <Link to="/modal" className="text-blue-500">
+        Modal
+      </Link>
+      <Link to="/counter" className="text-blue-500">
+        Counter
+      </Link>
+      <Link to="/reducer" className="text-blue-500">
+        CounterReducer
+      </Link>
+      <Link to="/immer" className="text-blue-500">
+        Counter Immer
+      </Link>
+      {/* Add link to your component page for HW */}
+    </Panel>
+  )
+}
+
+export default Navbar
